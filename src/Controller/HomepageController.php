1c350d84788f9b18ed736f109c159f2f25b63011
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-17 16:52:45 +0100

    I can see one room, but not all

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Room;
 
 class HomepageController extends AbstractController
 {
     #[Route('/homepage', name: 'homepage')]
-    public function index(): Response
+    public function showRooms(ManagerRegistry $doctrine): Response
     {
+        $data = $doctrine->getRepository(Room::class)->find(1);
+
+        $room = new Response('Check out this great room: '.$data->getName());
+
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
+            'room' => $room,
         ]);
     }
 }
