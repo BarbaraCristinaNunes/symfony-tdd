27fccf9b5714b19b3ec8d8758dc62f801e56d443
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-18 11:06:05 +0100

    now I can show all rooms

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -8,19 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Room;
+use App\Repository\RoomRepository;
 
 class HomepageController extends AbstractController
 {
     #[Route('/homepage', name: 'homepage')]
     public function showRooms(ManagerRegistry $doctrine): Response
     {
-        $data = $doctrine->getRepository(Room::class)->find(1);
+        $rooms = $doctrine->getRepository(Room::class)->findAll();
 
-        $room = new Response('Check out this great room: '.$data->getName());
+        // var_dump("data: ", $data[0]['name']);
+
+        // $room = new Response('Check out this great room: '.$data->getName());
+
+        // var_dump("room: ", $room);
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
-            'room' => $room,
+            'rooms' => $rooms,
         ]);
     }
 }
