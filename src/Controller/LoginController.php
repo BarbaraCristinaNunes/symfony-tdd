0d5cd59a9fa6cd50b8147df99a4c7168b57ccdc7
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-18 17:01:11 +0100

    I am trying to check if the user exist in database. It is not done

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,33 +7,58 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Entity\User;
+
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'login')]
     public function index(): Response
     {
+        $message = "";
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'message' => $message,
         ]);
     }
     
     #[Route('/userLogin', name: 'userLogin')]
-    public function userLogin(Request $request):  RedirectResponse
+    public function checkUserLogin(Request $request, ManagerRegistry $doctrine, Session $session):  Response
     {
         $email = "";
         $password = "";
+        $message = "";
 
-        if($request->request->get('emal') !== "" && $request->request->get('password') !== ""){
+        if($request->request->get('emal') === "" || $request->request->get('password') === ""){
+            $message = "No field can be empty!";
+
+            return $this->render('login/index.html.twig', [
+                'controller_name' => 'LoginController',
+                'message' => $message,
+            ]);
+
+        }else{
+
             $email = $request->request->get('emal');
             $password = $request->request->get('password');
-            echo "ola";
-            return $this->redirectToRoute('homepage');
-        }else{
-            echo "no";
-            return $this->redirectToRoute('login');
-        }
-        
-        
+            $db = $doctrine->getRepository(User::class)->find($email);
+
+            if($db !== null || new Response($db->getPassword()) !== $password){
+                $message = "Email  or password is not correct!";
+    
+                return $this->render('login/index.html.twig', [
+                    'controller_name' => 'LoginController',
+                    'message' => $message,
+                ]);
+    
+            }else{
+                $session->set('userId', new Response(db->getId()));
+    
+                return $this->render('homepage/index.html.twig');
+            }
+        }     
     }
 }
