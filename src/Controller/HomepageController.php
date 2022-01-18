ab932f1e5f0f0f0a2e235e48b54ce3ad09775f97
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-18 14:55:21 +0100

    created dropdown to show and select a room

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Room;
@@ -21,5 +23,14 @@ class HomepageController extends AbstractController
             'controller_name' => 'HomepageController',
             'rooms' => $rooms,
         ]);
+    }
+
+    #[Route('/booking', name: 'booking')]
+    public function goToRoom(Request $request, Session $session): RedirectResponse
+    {
+        $room = $request->request->get('room');
+
+        var_dump($room);
+        return $this->redirectToRoute('homepage');
     }
 }
