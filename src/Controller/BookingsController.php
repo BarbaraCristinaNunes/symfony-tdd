51a8290d0c07e9acbc22bb3aadd57a88e69299b6
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-20 11:28:40 +0100

    startd booking controller

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use App\Entity\Room;
 
 class BookingsController extends AbstractController
 {
     #[Route('/bookings', name: 'bookings')]
     public function index(): Response
     {
+
+        $roomId = $request->request->get('room');
+        $room = $doctrine->getRepository(Room::class)->findOneBy(array('id' => $roomId));
+        
         return $this->render('bookings/index.html.twig', [
             'controller_name' => 'BookingsController',
         ]);
