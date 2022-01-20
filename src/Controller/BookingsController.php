3e5b786267b3f6a04b6e7d3f244d4b309bc2eda3
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-20 14:42:39 +0100

    I am trying to check dates from database and post but my foreach is wrong

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -5,8 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
-use App\Entity\Room;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
+use App\Entity\Bookings;
+use App\Repository\RoomRepository;
+use App\Repository\UserRepository;
 
 class BookingsController extends AbstractController
 {
@@ -14,11 +20,32 @@ class BookingsController extends AbstractController
     public function index(): Response
     {
 
-        $roomId = $request->request->get('room');
-        $room = $doctrine->getRepository(Room::class)->findOneBy(array('id' => $roomId));
-        
         return $this->render('bookings/index.html.twig', [
             'controller_name' => 'BookingsController',
         ]);
+    }
+
+    #[Route('/request', name: 'request')]
+    public function requestBooking(Request $request, ManagerRegistry $doctrine, Session $session): Response
+    {
+        $date = $request->request->get(date);
+        $userStartTime = $request->request->get(startTime);
+        $userEndTime = $request->request->get(endTime);
+
+        $room = $session->get("roomId");
+
+        $roomBookings = $doctrine->getRepository(Bookings::class)->findAllByRoomId();
+
+        foreach( $roomBooking in $roomBookings){
+
+            $dbDate = $roomBooking.startDay;
+
+            if($dbDate == $date){
+
+                $dbStartTime = $roomBooking.startTime;
+                $dbEndTime = $roomBooking.endTime;
+            }
+        }
+
     }
 }
