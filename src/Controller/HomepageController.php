2823854932f1f1ee4024955fe0cca9feb5e67b2b
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-01-20 11:27:08 +0100

    Now I have access to user premium information through session, I get room information and send user to bookings

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -16,67 +16,51 @@ use App\Repository\RoomRepository;
 class HomepageController extends AbstractController
 {
     #[Route('/homepage', name: 'homepage')]
-    public function showRoomsAndUsers(ManagerRegistry $doctrine): Response
+    public function showRoomsAndUsers(ManagerRegistry $doctrine, Session $session): Response
     {
         $rooms = $doctrine->getRepository(Room::class)->findAll();
-        $users = $doctrine->getRepository(User::class)->findAll();
+        // $users = $doctrine->getRepository(User::class)->findAll();
         $message = "";
 
         return $this->render('homepage/index.html.twig', [
             'controller_name' => 'HomepageController',
             'rooms' => $rooms,
-            'users' => $users,
+            'user' => "Hello ". $session->get('userName'),
             'message' => $message,
         ]);
     }
 
     #[Route('/booking', name: 'booking')]
-    public function roomValidation(Request $request, ManagerRegistry $doctrine): Response
+    public function roomValidation(Request $request, ManagerRegistry $doctrine, Session $session): Response
     {
 
         $rooms = $doctrine->getRepository(Room::class)->findAll();
-        $users = $doctrine->getRepository(User::class)->findAll();
         $message = "";
 
         $roomId = $request->request->get('room');
-        $userId = $request->request->get('user');
-
         $room = $doctrine->getRepository(Room::class)->findOneBy(array('id' => $roomId));
-        $user = $doctrine->getRepository(User::class)->findOneBy(array('id' => $userId));
 
-        var_dump($room->getPremiumMember());
-        var_dump($user->getPremiumMember());
+        $userPremium = $session->get('Premium');
 
-        if($room->getPremiumMember() == true && $user->getPremiumMember() == false){
+        // var_dump($room->getPremiumMember());
+        // var_dump($user->getPremiumMember());
+
+        if($room->getPremiumMember() == true && $userPremium == false){
             $message = "You can not booking this room!";
 
             return $this->render('homepage/index.html.twig', [
                 'controller_name' => 'HomepageController',
                 'rooms' => $rooms,
-                'users' => $users,
+                'user' => "Hello ". $session->get('name'),
                 'message' => $message,
             ]);
+        }else{
+
+            $session->set('roomId', $roomId);
+
+            return $this->render('bookings/index.html.twig', [
+                'controller_name' => 'BookingsController',
+            ]);
         }        
-
-        // return $this->goToRoom($room);
     }
-
-    // public function goToRoom($room): RedirectResponse
-    // {
-    //     $roomId = $request->request->get('room');
-    //     $userId = $request->request->get('user');
-    //     $message = "";
-
-    //     var_dump($roomPremium);
-    //     var_dump($userPremium);
-
-    //     $room = $doctrine->getRepository(Room::class)->findOneBy(array('id' => $roomId));
-    //     $user = $doctrine->getRepository(User::class)->findOneBy(array('id' => $userId));
-
-    //     if($room->getPremiumMember() == 0 && $user->getPremiumMember() == 1){
-    //         $message = "You can not booking this room!";
-    //     }        
-
-    //     // return $this->redirectToRoute('bookings');
-    // }
 }
